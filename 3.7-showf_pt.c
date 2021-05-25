d7c0b2e264738b3ee52e2e358ef8a35358cf9997
Zane <zane.zhao@chinaentropy.com>
2021-05-25 18:02:41 +0800

    3.5 使用数据类型

--- 3.7-showf_pt.c
+++ 3.7-showf_pt.c
@@ -6,6 +6,9 @@ int main(void)
     double abet = 2.14e9;
     long double dip = 5.32e-5;
     printf("浮点数：%f 科学计数法表示：%e\n", aboat, aboat);
+    // 下一行要求编译器支持C99或其中的相关特性
+    printf("十六进制表示：%a\n", aboat);
+    printf("浮点数：%f 科学计数法表示：%e\n", abet, abet);
+    printf("浮点数：%Lf 科学计数法表示：%Le\n", dip, dip);
     return 0;
-    // C(())
 }
